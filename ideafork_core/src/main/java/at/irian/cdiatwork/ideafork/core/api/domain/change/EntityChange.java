c4f433732c5952c2c03c0099618fa1dfc3fc9a43
Gerhard Petracek <gerhard.petracek@irian.at>
2014-12-08 15:45:09 +0100

    renamed package

---
+++ ideafork_core/src/main/java/at/irian/cdiatwork/ideafork/core/api/domain/change/EntityChange.java
@@ -0,0 +1,35 @@
+package at.irian.cdiatwork.ideafork.core.api.domain.change;
+
+import at.irian.cdiatwork.ideafork.core.api.domain.BaseEntity;
+
+public class EntityChange extends BaseEntity {
+    private static final long serialVersionUID = -6314134718971162163L;
+
+    private final String entityId; //just for an easier selection
+    private final long entityVersion;
+    private final String entityState;
+    private final long changeTimestamp;
+
+    public EntityChange(String id, long version, String entityState, long changeTimestamp) {
+        this.entityId = id;
+        this.entityVersion = version;
+        this.entityState = entityState;
+        this.changeTimestamp = changeTimestamp;
+    }
+
+    public String getEntityId() {
+        return entityId;
+    }
+
+    public long getEntityVersion() {
+        return entityVersion;
+    }
+
+    public String getEntityState() {
+        return entityState;
+    }
+
+    public long getChangeTimestamp() {
+        return changeTimestamp;
+    }
+}
